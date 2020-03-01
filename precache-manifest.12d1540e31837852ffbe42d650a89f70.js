f7fd96a98f4d4947bd5fec364827fe8990136416
jenniekibiri <josphatje@gmail.com>
2020-03-01 18:42:33 +0300

    Updates

---
+++ precache-manifest.12d1540e31837852ffbe42d650a89f70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "81fa48a4eacd51506868",
+    "url": "/jenniekibiri/react-portfolio/static/css/main.cef42acc.chunk.css"
+  },
+  {
+    "revision": "81fa48a4eacd51506868",
+    "url": "/jenniekibiri/react-portfolio/static/js/main.81fa48a4.chunk.js"
+  },
+  {
+    "revision": "60123046c69ad5cd1e5d",
+    "url": "/jenniekibiri/react-portfolio/static/css/1.8bc228b4.chunk.css"
+  },
+  {
+    "revision": "60123046c69ad5cd1e5d",
+    "url": "/jenniekibiri/react-portfolio/static/js/1.60123046.chunk.js"
+  },
+  {
+    "revision": "8a03ca0954fd847c1282",
+    "url": "/jenniekibiri/react-portfolio/static/js/runtime~main.8a03ca09.js"
+  },
+  {
+    "revision": "bb9bda49422881d85f314d0c80e9b9ca",
+    "url": "/jenniekibiri/react-portfolio/static/media/yeoman.bb9bda49.png"
+  },
+  {
+    "revision": "ff9cbe44d1dc3ff609eea6a2c15a5c6b",
+    "url": "/jenniekibiri/react-portfolio/static/media/yuna.ff9cbe44.jpg"
+  },
+  {
+    "revision": "9a7022759a7ed3450fbcb1dd2ff8bb84",
+    "url": "/jenniekibiri/react-portfolio/index.html"
+  }
+];
